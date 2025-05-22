7feacde4168210737e79702379378b0ca7b04c62
SakshamRijal1 <sakshamrijal07@gmail.com>
2025-05-22 19:09:50 +0545

    Your commit message

---
+++ varandcomm.cpp
@@ -0,0 +1,10 @@
+#include<iostream>//This is the comment for header file
+using namespace std;//using namespace
+int main(){
+  int a=6;//variable declearing (a is vaiable)
+  cout<<"The number is "<<a <<endl;
+  char b[40]="SAKSHAM";
+  cout<<"The data store din variable b is "<<b;
+  return 0;
+
+}
