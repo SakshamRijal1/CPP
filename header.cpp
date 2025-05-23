e3118b83a417b86b34be6b4d2f0b43abe3d70e2f
SakshamRijal1 <sakshamrijal07@gmail.com>
2025-05-23 13:30:59 +0545

    Header understand

---
+++ header.cpp
@@ -0,0 +1,12 @@
+/*Two type of header file 
+1) System header file
+2)User defined header file*/
+#include<iostream>//system header file
+#include "this.h"//this is user defined header filea(But make sure there is a file named this.h)
+using namespace std;
+int main()
+{
+  cout<<"This is nothing "; 
+  return 0;
+
+}
