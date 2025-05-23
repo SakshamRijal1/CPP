3199d256e89fd78d7a156373151cf30a164d430b
SakshamRijal1 <sakshamrijal07@gmail.com>
2025-05-23 17:17:32 +0545

    conv

---
+++ conversion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float a=24.4;
+  cout<<"The value of a is"<<a;
+  cout<<"The size of a is"<<sizeof(24.4F)<<endl;
+
+  long double b=56.6L;
+    cout<<"The size of a is"<<sizeof(56.6)<<endl;
+    cout<<"The size of a is"<<sizeof(56.6L)<<endl;
+    cout<<"The size of a is"<<sizeof(56.6l)<<endl;
+
+  cout<<b;
+
+
+}
